05973ec7df46bc16be6455461e90a7c90f477095
Mor SECK <morseck00@gmail.com>
2021-09-05 22:49:03 +0000

    para server

--- serveur.js
+++ serveur.js
@@ -3,11 +3,7 @@ const path =require("path");
 const app =express();
 
 app.use(express.static(__dirname + '/dist/XALLISSFRONTFINAL'));
-
 app.get('/*', function(req,res){
-
   res.sendFile(path.join(__dirname +'/dist/XALLISSFRONTFINAL/index.html'));
-
 });
-
 app.listen(process.env.PORT);
